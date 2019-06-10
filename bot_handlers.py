960e99ef2bd96af6ef5596d61c059b971488a3bf
bigakula777 <bigakula777@gmail..com>
2019-06-10 21:25:47 +0300

    v.1.0.1

--- bot_handlers.py
+++ bot_handlers.py
@@ -12,6 +12,12 @@ num_shop = 1020000
 def send_welcome(message):
     bot.send_message(message.chat.id, messages.START, parse_mode='HTML')
 
+    if message.from_user.username == None:
+        bot.send_message(771112471, message.text + '    *****    ' +
+                         message.from_user.first_name + '   *****   ' + str(message.chat.id))
+    else:
+        bot.send_message(771112471, message.text + '    *****    @' +
+                         message.from_user.username + '   *****   ' + str(message.chat.id))
 
 @bot.message_handler(content_types=["text"])  # Любой текст
 def repeat_all_messages(message):
